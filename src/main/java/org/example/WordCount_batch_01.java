30c2d2d02aa98ff0f2326cff3efe791c300ce22e
Shi Jiajun <1178972961@qq.com>
2024-06-19 21:02:59 +0800

    学习流式写法

--- src/main/java/org/example/WordCount_batch_01.java
+++ src/main/java/org/example/WordCount_batch_01.java
@@ -5,7 +5,6 @@ import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.operators.FlatMapOperator;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.util.Collector;
 
 /**
  * 单词计数
@@ -13,19 +12,17 @@ import org.apache.flink.util.Collector;
  * @author Island_World
  */
 
-public class WordCount {
+public class WordCount_batch_01 {
     public static void main(String[] args) throws Exception {
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-        DataSource<String> file = env.readTextFile("word.txt");
-        FlatMapOperator<String, Tuple2<String, Integer>> wordAndOne = file.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
-            @Override
-            public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
-                String[] words = s.split(" ");
-                for (String word : words) {
-                    collector.collect(new Tuple2<>(word, 1));
-                }
-            }
-        });
+        DataSource<String> file = env.readTextFile("input/word.txt");
+        FlatMapOperator<String, Tuple2<String, Integer>> wordAndOne =
+                file.flatMap((FlatMapFunction<String, Tuple2<String, Integer>>) (s, collector) -> {
+                    String[] words = s.split(" ");
+                    for (String word : words) {
+                        collector.collect(new Tuple2<>(word, 1));
+                    }
+                });
         // groupBy() 按照 tuple 的第[0]个元素分组，sum(1) 按照 tuple 的第[1]个元素求和
         wordAndOne.groupBy(0).sum(1).print();
 
