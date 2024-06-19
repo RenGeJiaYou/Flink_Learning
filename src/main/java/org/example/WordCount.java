9c6b3a6839f4d94b9ff000e87e7d638f3d430a55
sjj <shijiajunsjj@outlook.com>
2024-06-19 14:01:14 +0800

    6.19 开始学习 Flink

--- src/main/java/org/example/WordCount.java
+++ src/main/java/org/example/WordCount.java
@@ -1,10 +1,33 @@
 package org.example;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
 /**
  * 单词计数
- * 
- * @author Island_World 
+ *
+ * @author Island_World
  */
 
 public class WordCount {
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSource<String> file = env.readTextFile("word.txt");
+        FlatMapOperator<String, Tuple2<String, Integer>> wordAndOne = file.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                String[] words = s.split(" ");
+                for (String word : words) {
+                    collector.collect(new Tuple2<>(word, 1));
+                }
+            }
+        });
+        // groupBy() 按照 tuple 的第[0]个元素分组，sum(1) 按照 tuple 的第[1]个元素求和
+        wordAndOne.groupBy(0).sum(1).print();
+
+    }
 }
