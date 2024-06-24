a178b2004423ab21a8b8a7cc59d4da94693f0fa7
sjj <shijiajunsjj@outlook.com>
2024-06-24 13:42:31 +0800

    比对流批处理代码的不同

--- src/main/java/org/example/WordCount_stream_02.java
+++ src/main/java/org/example/WordCount_stream_02.java
@@ -2,8 +2,9 @@ package org.example;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
 /**
@@ -15,16 +16,16 @@ import org.apache.flink.util.Collector;
 public class WordCount_stream_02 {
     public static void main(String[] args) throws Exception {
         //创建执行环境
-        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         //从文件中读取数据
         String inputPath = "input/word.txt";
-        DataSet<String> inputDataSet = env.readTextFile(inputPath);
+        DataStream<String> inputDataStream = env.readTextFile(inputPath);
 
         // 对数据集进行处理，按空格分词展开，转换成(word, 1)二元组进行统计
-        // 按照第一个位置的 word 分组
+        // 按照第一个位置的 word 分组，item 是 Tuple2 的一个实例，f0 是第一个位置的数据，f1 是第二个位置的数据
         // 按照第二个位置上的数据求和
-        DataSet<Tuple2<String, Integer>> resultSet = inputDataSet.flatMap(new FlatMapFunction<String, Tuple2<String,Integer>>() {
+        DataStream<Tuple2<String, Integer>> dataStream = inputDataStream.flatMap(new FlatMapFunction<String, Tuple2<String,Integer>>() {
                     @Override
                     public void flatMap(String s, Collector<Tuple2<String, Integer>> out) throws Exception {
                         // 按空格分词
@@ -35,9 +36,24 @@ public class WordCount_stream_02 {
                         }
                     }
                 })
-                .groupBy(0)
+                .keyBy(item -> item.f0)
                 .sum(1);
-        resultSet.print();
+        dataStream.print();
+
+        //执行任务
+        env.execute();
+
+        /*
+        * 流处理会将所有中间过程都会被输出，前面的序号就是并行执行任务的线程编号。输出为：
+        * 3> (hello,1)
+        * 7> (flink,1)
+        * 5> (world,1)
+        * 3> (hello,2)
+        * 1> (kafka,1)
+        * 8> (hadoop,1)
+        * 3> (hello,3)
+        * 1> (spark,1)
+        * */
     }
 }
 
