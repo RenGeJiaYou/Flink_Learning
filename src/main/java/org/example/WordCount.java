a5e4b0e545d8f497c24fe8a338c2fb91c54bb186
sjj <shijiajunsjj@outlook.com>
2024-06-24 11:27:22 +0800

    简化代码

--- src/main/java/org/example/WordCount.java
+++ src/main/java/org/example/WordCount.java
@@ -25,24 +25,20 @@ public class WordCount {
         // 对数据集进行处理，按空格分词展开，转换成(word, 1)二元组进行统计
         // 按照第一个位置的word分组
         // 按照第二个位置上的数据求和
-        DataSet<Tuple2<String, Integer>> resultSet = inputDataSet.flatMap(new MyFlatMapper())
+        DataSet<Tuple2<String, Integer>> resultSet = inputDataSet.flatMap(new FlatMapFunction<String, Tuple2<String,Integer>>() {
+                    @Override
+                    public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                        // 按空格分词
+                        String[] words = s.split(" ");
+                        // 遍历所有word，包成二元组输出
+                        for (String word : words) {
+                            collector.collect(new Tuple2<>(word, 1));
+                        }
+                    }
+                })
                 .groupBy(0)
                 .sum(1);
         resultSet.print();
-    }
-
-    // 自定义类，实现FlatMapFunction接口
-    public static class MyFlatMapper implements FlatMapFunction<String, Tuple2<String, Integer>> {
-
-        @Override
-        public void flatMap(String s, Collector<Tuple2<String, Integer>> out) throws Exception {
-            // 按空格分词
-            String[] words = s.split(" ");
-            // 遍历所有word，包成二元组输出
-            for (String str : words) {
-                out.collect(new Tuple2<>(str, 1));
-            }
-        }
     }
 }
 
