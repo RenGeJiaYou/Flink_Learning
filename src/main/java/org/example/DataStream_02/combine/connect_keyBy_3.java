cf1314a4fbdbd2572b84227f1612f155adfdeb07
sjj <shijiajunsjj@outlook.com>
2024-07-03 14:11:48 +0800

    7.3 完全理解了 keyBy 的示例代码

--- src/main/java/org/example/DataStream_02/combine/connect_keyBy_3.java
+++ src/main/java/org/example/DataStream_02/combine/connect_keyBy_3.java
@@ -18,6 +18,12 @@ import java.util.Map;
  * keyBy 合流操作<p>
  * 连接两条流，输出能根据id匹配上的数据（类似inner join效果）<p>
  * <p>
+ * 第一步：用 source1.connect(source2) 连接两条流
+ * 第二步：用 connect.keyBy(s1 -> s1.f0, s2 -> s2.f0) 根据关联条件进行 keyBy
+ * 第三步※：用 keyByCoStream.process() 实现互相匹配的效果
+ * - 如果A B 两条流中有 id 一致的两条数据，就输出这两条数据,但是谁的数据先来是不一定的
+ * - 为此需要为两个流各设立一条缓存，用来存储各自的数据，A 流来一条数据时，就将其存入 A 缓存中，然后查看 B 缓存中是否有匹配的数据就一起输出；反之亦然。
+ * <p>
  * todo 这一部分不太懂
  *
  * @author Island_World
@@ -37,13 +43,14 @@ public class connect_keyBy_3 {
         DataStreamSource<Tuple3<Integer, String, Integer>> source2 = env.fromElements(
                 Tuple3.of(1, "甲一", 1),
                 Tuple3.of(1, "甲二", 2),
-                Tuple3.of(2, "丙", 1),
-                Tuple3.of(3, "丁", 1)
+                Tuple3.of(2, "乙", 1),
+                Tuple3.of(3, "丙", 1)
         );
 
         ConnectedStreams<
                 Tuple2<Integer, String>,
-                Tuple3<Integer, String, Integer>> connect = source1.connect(source2);
+                Tuple3<Integer, String, Integer>
+                > connect = source1.connect(source2);
 
         // 多并行度下，需要根据关联条件进行 keyby，才能保证 key相同的数据到一起去，才能匹配上
         // ConnectedStreams -- keyBy --> 还是 ConnectedStreams
@@ -56,6 +63,7 @@ public class connect_keyBy_3 {
                     Map<Integer, List<Tuple2<Integer, String>>> s1Cache = new HashMap<>();
                     Map<Integer, List<Tuple3<Integer, String, Integer>>> s2Cache = new HashMap<>();
 
+                    /** 第一条流的处理逻辑 */
                     @Override
                     public void processElement1(Tuple2<Integer, String> value, Context ctx, Collector<String> out) throws Exception {
                         Integer id = value.f0;
@@ -72,7 +80,7 @@ public class connect_keyBy_3 {
 
                         if (s2Cache.containsKey(id)) {
                             for (Tuple3<Integer, String, Integer> s2Value : s2Cache.get(id)) {
-                                out.collect("s1" + value + " -> " + s2Value);
+                                out.collect("processElement1():  s1:" + value + " -> " + s2Value);
                             }
                         }
                     }
@@ -91,9 +99,9 @@ public class connect_keyBy_3 {
                             s2Cache.get(id).add(value);
                         }
 
-                        if (s2Cache.containsKey(id)) {
+                        if (s1Cache.containsKey(id)) {
                             for (Tuple2<Integer, String> s1Element : s1Cache.get(id)) {
-                                out.collect("s1:" + s1Element + "<========>" + "s2:" + value);
+                                out.collect("processElement2():  s1:" + s1Element + "<========>" + "s2:" + value);
                             }
                         }
                     }
